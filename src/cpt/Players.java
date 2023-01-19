e80c10c17fb2c2ac8eb9410ad8a310d80fdf869e
AlexYounggg <alex.young23@ycdsbk12.ca>
2023-01-19 15:06:44 -0500

    Constructor for data

---
+++ src/cpt/Players.java
@@ -0,0 +1,46 @@
+package cpt;
+
+public class Players {
+    
+    private int rank;
+    private String name;
+    private int threesMade;
+    private int threesAttempted;
+    private double threePercent;
+
+    public Players (int rank, String name, int threesMade, int threesAttempted, double threePercent) {
+        this.rank = rank;
+        this.name = name;
+        this.threesMade = threesMade;
+        this.threesAttempted = threesAttempted;
+        this.threePercent = threePercent;
+      
+        
+    }
+
+    public int geRank() {
+        return this.rank;
+
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public int getThreesMade() {
+        return this.threesMade;
+
+    }
+
+    public int getThreesAttempted() {
+        return this.threesAttempted;
+
+    }
+    
+    public Double getThreePercent() {
+        return this.threePercent;
+    }
+
+   
+    
+}
