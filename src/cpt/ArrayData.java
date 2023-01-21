94b5bfbacaa9063b082ce190fd0278842c522295
AlexYounggg <alex.young23@ycdsbk12.ca>
2023-01-21 15:30:07 -0500

    Editting filereader

--- src/cpt/ArrayData.java
+++ src/cpt/ArrayData.java
@@ -5,12 +5,12 @@ import java.io.*;
 
 public class ArrayData {
 
-    public static void main(String[] args) {
-
-String line = "";
-int count = 0;
-String csvSplitBy = ",";
-ArrayList<Players> stats = new ArrayList<>();
+public String line = "";
+public int count = 0;
+public String csvSplitBy = ",";
+public ArrayList<Players> stats = new ArrayList<>();
+    
+    public void dataReader(){
 
     try {
 BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Alex Young\\git\\cpt-Alexyounggg\\src\\cpt\\ThreePointPercent.csv\\" )); 
@@ -20,12 +20,12 @@ while ((line = reader.readLine()) != null) {
     String[] row = line.split(csvSplitBy);
 
     if (count != 0){
-        for (int i = 0; i < row.length; i+=13){
+        for (int i = 0; i < row.length; i++){
             int rank = Integer.parseInt(row[0]);
             String name = row [1];
             int threesMade = Integer.parseInt(row[2]);
             int threesAttempted = Integer.parseInt(row[3]);
-            int threesPercent = Integer.parseInt(row[4]);
+            double threesPercent = Double.parseDouble(row[4]);
             Players numbers = new Players (rank, name, threesMade, threesAttempted, threesPercent);
             stats.add(numbers);
 
@@ -40,6 +40,16 @@ while ((line = reader.readLine()) != null) {
     }
 
  
+    
+    }
+    public int getLength(){
+        return stats.size();
+    }
+
+    public ArrayList<Players> getArray(){
+        return this.stats;
     }
 }
+
+
 
