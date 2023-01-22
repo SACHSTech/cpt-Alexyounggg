69ad3724793cf8101535a64565cf910d7248c211
AlexYounggg <alex.young23@ycdsbk12.ca>
2023-01-22 01:21:39 -0500

    Attemptign to add more functionality

--- src/cpt/Test.java
+++ src/cpt/Test.java
@@ -16,9 +16,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.input.MouseEvent;
 
 
 public class Test extends Application {
+
+    private Label label;
 
     public static void main(String[] args) {
         launch(args);
@@ -29,16 +32,24 @@ public class Test extends Application {
         // Define the x and y axis
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
+        NumberAxis xxAxis = new NumberAxis();
+        NumberAxis yyAxis = new NumberAxis();
         xAxis.setLabel("Three Point Makes");
         yAxis.setLabel("Thre Point Percent");
+        xxAxis.setLabel("Three Point Attemps");
+        yyAxis.setLabel("Thre Point Percent");
 
         // Create the line chart
         ScatterChart<Number, Number> scatterChart = new ScatterChart<>(xAxis, yAxis);
-        scatterChart.setTitle("Data Plot");
+        ScatterChart<Number, Number> newScatterChart = new ScatterChart<>(xxAxis, yyAxis);
+        scatterChart.setTitle("Three Point Makes vs Percent");
+        newScatterChart.setTitle("Three Point Attempts vs Makes");
 
         // Create a data series to hold the data
         XYChart.Series<Number, Number> dataSeries = new XYChart.Series<>();
+        XYChart.Series<Number, Number> newDataSeries = new XYChart.Series<>();
         dataSeries.setName("Best shooters");
+        newDataSeries.setName("Best shooters");
 
         // Read data from csv file and add it to the data series
         try {
@@ -47,7 +58,8 @@ public class Test extends Application {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] values = line.split(",");
-                dataSeries.getData().add(new XYChart.Data<>(Integer.parseInt(values[2]), Double.parseDouble(values[4])));
+                dataSeries.getData().add(new XYChart.Data<>(Integer.parseInt(values[2]), Double.parseDouble(values[4]), values[1]));
+                newDataSeries.getData().add(new XYChart.Data<>(Integer.parseInt(values[3]), Double.parseDouble(values[4]), values[1]));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -55,9 +67,18 @@ public class Test extends Application {
 
         // Add the data series to the line chart
         scatterChart.getData().add(dataSeries);
+        newScatterChart.getData().add(newDataSeries);
 
-        StackPane spLineChart = new StackPane();
-        spLineChart.getChildren().add(scatterChart);
+        for (XYChart.Data<Number, Number> data : dataSeries.getData()) {
+            StackPane stackPane = (StackPane) data.getNode();
+            Label label = new Label(.getName());
+            stackPane.getChildren().add(label);
+        }
+
+        label = new Label();
+
+        newScatterChart.setVisible(false);
+        
 
          // Create a button to display another graph
          Button button = new Button("Show Another Graph");
@@ -65,39 +86,22 @@ public class Test extends Application {
              @Override
              public void handle(ActionEvent event) {
 
-                dataSeries.getData().clear();
-
-                 
-                 xAxis.setLabel("ThreePoint Attempts");
-                 yAxis.setLabel("ThreePoint Makes");
-
-            
-                 scatterChart.setTitle("Another Data Plot");
- 
- 
-                 // Read data from a new csv file and add it to the new data series
-                 try {
-                    File file = new File("C:\\Users\\Alex Young\\git\\cpt-Alexyounggg\\src\\cpt\\ThreePointPercentEditted.csv\\");
-                    Scanner scanner = new Scanner(file);
-                    while (scanner.hasNextLine()) {
-                        String line = scanner.nextLine();
-                        String[] values = line.split(",");
-                        dataSeries.getData().add(new XYChart.Data<>(Integer.parseInt(values[3]), Double.parseDouble(values[4])));
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                if (scatterChart.isVisible()) {
+                    scatterChart.setVisible(false);
+                    newScatterChart.setVisible(true);
+                } else {
+                    scatterChart.setVisible(true);
+                    newScatterChart.setVisible(false);
                 }
- 
-                  // Add the data series to the line chart
-                 scatterChart.getData().add(dataSeries);  
-            
-    }
+            }
+    
      });
      
-     
+     StackPane spLineChart = new StackPane();
+     spLineChart.getChildren().addAll(scatterChart, newScatterChart);
+
      StackPane spButton = new StackPane();
         spButton.getChildren().add(button);
-
    
 
         VBox vbox = new VBox(5);
@@ -106,7 +110,7 @@ public class Test extends Application {
      
         
         Scene scene  = new Scene(vbox,800,600);
-        scatterChart.getData().add(dataSeries);       
+              
 
         primaryStage.setScene(scene);
         primaryStage.show();
